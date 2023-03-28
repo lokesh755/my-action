7e3c346e3995d5930ec8a00d12471387c3f3ae27
Anthony <86444119+anthms@users.noreply.github.com>
2023-03-28 17:01:38 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ const github = require('@actions/github');
 
 try {
   console.log(`Starting. . .`);
-  const payload = JSON.stringify(github.context.action_repository, undefined, 2)
+  const payload = JSON.stringify(github.context, undefined, 2)
   console.log(`Output: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
