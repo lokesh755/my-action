87b058860c1b25881f5da3723ebd328b7c77e539
Anthony <86444119+anthms@users.noreply.github.com>
2023-03-28 16:36:08 -0500

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,10 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+  console.log(`Starting. . .`);
+  const payload = JSON.stringify(github.context.action_repository, undefined, 2)
+  console.log(`Output: ${payload}`);
+} catch (error) {
+  core.setFailed(error.message);
+}
